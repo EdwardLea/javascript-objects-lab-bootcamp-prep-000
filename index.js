67a78b73773caa307531413c63847237e8f6102d
leaedward87@gmail.com <leaedward87@gmail.com>
2018-08-28 19:38:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function updateObjectWithKeyAndValue(recipe,key,value){
   return Object.assign({}, recipe, { [key]: value });
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(recipe,key1,value1){
-   recipe[key1] = [value1];
+function destructivelyUpdateObjectWithKeyAndValue(recipe,key1,value){
+   recipe[key] = [value1];
    return recipe
 }
