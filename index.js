a7e0c455210cdb2bef83f663c21d161ae7dceb54
leaedward87@gmail.com <leaedward87@gmail.com>
2018-08-28 19:20:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ function updateObjectWithKeyAndValue(recipe,key,value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipe,key,value){
-  return recipe[key] = ['value2'];
+   recipe[key] = ['value2'];
+   return recipe;
 }
