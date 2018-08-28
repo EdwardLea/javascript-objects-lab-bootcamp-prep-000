23611e9d7bbfa52845f447001f7660ab5760d1cb
leaedward87@gmail.com <leaedward87@gmail.com>
2018-08-28 20:04:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ function deleteFromObjectByKey(object,key){
   return Object.assign({}, object, { key })
 }
 
+function deleteFromObjectByKey(object,key){
+  var newObj = Object.assign({}, object, { key });
+}
+
 
 
 
